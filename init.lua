a9032736296103b484adcb30cda2c505859bbc46
Carlos Amaya <10285277+cramayar@users.noreply.github.com>
2024-08-08 16:48:31 +0800

    Animation: Add Cellular Automaton for relaxation

--- init.lua
+++ init.lua
@@ -723,6 +723,8 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  {'eandrju/cellular-automaton.nvim', },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -30,3 +30,5 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keybinding for recreational CelluarAutomaton make it rain
+vim.keymap.set('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>')
