b89628dad68c0d62adbc2a5a235b2c2164448464
Carlos Amaya <10285277+cramayar@users.noreply.github.com>
2024-08-08 16:29:10 +0800

    LSP: add codelldb in the ensure_installed list

--- init.lua
+++ init.lua
@@ -507,6 +507,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'codelldb', -- Used to format C code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
