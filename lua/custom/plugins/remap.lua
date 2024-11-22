4219e6b785f30398854b5b11ae4da80b0cfb7be4
Carlos Amaya <10285277+cramayar@users.noreply.github.com>
2024-11-23 01:08:09 +0800

    remap: explicit diagnostic navigation

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -25,6 +25,9 @@ vim.keymap.set('n', '<leader>tn', ':tabn<CR>')
 vim.keymap.set('n', '<leader>tp', ':tabp<CR>')
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]ror message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
