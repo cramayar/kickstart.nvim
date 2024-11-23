1a98c337061194df0e14318ace03c348b3d2bec7
Carlos Amaya <10285277+cramayar@users.noreply.github.com>
2024-11-23 09:46:49 +0800

    colors: configure additional settings in tokyonight

--- init.lua
+++ init.lua
@@ -686,6 +686,7 @@ require('lazy').setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
         style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = 'day', -- The theme is used when the background is set to light
         transparent = true, -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         styles = {
@@ -697,6 +698,11 @@ require('lazy').setup({
           sidebars = 'dark', -- style for sidebars, see below
           floats = 'dark', -- style for floating windows
         },
+        day_brightness = 0.3, -- Adjust the brightnes of the color of the "Day" style
+        dim_inactive = false, -- dims inactive window
+        lualine_bold = false, -- When `true` section headers in the lua line will be bold.
+        on_colors = function(colors) end,
+        on_highlights = function(highlights, colors) end,
       }
     end,
   },
@@ -812,6 +818,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
   require 'custom.plugins.undotree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
