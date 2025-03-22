4e6a10fab6e859f99aecc0ffe4ef444953004d31
Carlos Amaya <10285277+cramayar@users.noreply.github.com>
2025-03-22 23:24:07 +0800

    futigive: remove quiet flag from git push keymap

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -6,7 +6,7 @@ return {
       vim.keymap.set('n', '<leader>gs', ':Git<CR>', { noremap = true, desc = 'git status' }) --git status
       vim.keymap.set('n', '<leader>ga', ':Git add ', { noremap = true, desc = 'git add ' })
       vim.keymap.set('n', '<leader>gA', ':Git add .<CR>', { noremap = true, desc = 'git add .' })
-      vim.keymap.set('n', '<leader>gp', ':Git push --quiet <CR>', { noremap = true, desc = 'git push' })
+      vim.keymap.set('n', '<leader>gp', ':Git push <CR>', { noremap = true, desc = 'git push' })
       vim.keymap.set('n', '<leader>gc', ':Git commit -qm "', { noremap = true, desc = 'git commit -am' })
       vim.keymap.set('n', '<leader>gL', ':Git log <CR>', { noremap = true, desc = 'git log' })
       vim.keymap.set('n', '<leader>gl', ':Git log --oneline<CR>', { noremap = true, desc = 'git log --oneline' })
